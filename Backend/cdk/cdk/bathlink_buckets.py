e95c3625df13c58550044105581cb479a98bb9db
Jonah Kaplan <jonahkaplan@hotmail.com>
2025-03-25 17:42:05 +0000

    Added Bucket

---
+++ Backend/cdk/cdk/bathlink_buckets.py
@@ -0,0 +1,21 @@
+from aws_cdk import aws_s3 as s3
+
+
+class BathLinkBuckets:
+
+    def __init__(self):
+        self.pfp_bucket = None
+
+    def create_buckets(self, stack):
+        self.pfp_bucket = s3.Bucket(
+            stack, "BathLinkPFPBucket",
+            bucket_name="bathlink-pfp",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ACLS
+        )
+
+
+
+
+
+
+
