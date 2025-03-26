80607bbcd54a13326ce70dfab17be162aafdb6cf
svn27 <svn27@bath.ac.uk>
2025-03-26 16:00:28 +0000

    added unit tests for user lambda and also fixed user lambda

--- Backend/tests/unit/test_manage_users_function.py
+++ Backend/tests/unit/test_manage_users_function.py
@@ -12,28 +12,126 @@ def dynamodb_setup():
         table = dynamodb.create_table(
             TableName="users-table",
             KeySchema=[{"AttributeName": "student-id", "KeyType": "HASH"}],
-            AttributeDefinitions=[{"AttributeName": "student-id", "AttributeType": "S"}],
+            AttributeDefinitions=[
+                {"AttributeName": "student-id", "AttributeType": "S"}
+            ],
             ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+        )
+
+        table.put_item(
+            Item={
+                "student-id": "test-user-id",
+                "calendar": {"busy": []},
+                "email": "test@test.com",
+                "phone": "+123456789",
+                "name": "Martin Power",
+                "dob": "13/12/13",
+                "profile": {
+                    "social": [],
+                    "description": "Tall basketballer",
+                    "pronouns": "he/him",
+                },
+                "matchPreferences": {"enabled": False, "activities": []},
+            }
         )
         yield table
 
 
 # Import lambda functions after initialising mocked dynamodb
-from Backend.lambda_functions.manage_users_lambda.lambda_function import PostConfirmation
+from Backend.lambda_functions.manage_users_lambda.lambda_function import (
+    PostConfirmation,
+)
+from Backend.lambda_functions.manage_users_lambda.lambda_function import lambda_handler
+
+
+@pytest.mark.parametrize(
+    "student_id, expected_status, expected_response",
+    [
+        (
+            "test-user-id",
+            200,
+            {
+                "student-id": "test-user-id",
+                "calendar": {"busy": []},
+                "email": "test@test.com",
+                "phone": "+123456789",
+                "name": "Martin Power",
+                "dob": "13/12/13",
+                "profile": {
+                    "social": [],
+                    "description": "Tall basketballer",
+                    "pronouns": "he/him",
+                },
+                "matchPreferences": {"enabled": False, "activities": []},
+            },
+        ),
+        ("nonexistent", 404, "userId:nonexistent not found!"),
+    ],
+)
+@pytest.mark.benchmark(group="Manage Users")
+def test_get_user(
+    benchmark, dynamodb_setup, student_id, expected_status, expected_response
+):
+    event = {"httpMethod": "GET", "pathParameters": {"userId": student_id}}
+
+    response = benchmark(lambda_handler, event, None)
+    assert response["statusCode"] == expected_status
+    assert json.loads(response["body"]) == expected_response
+
+
+@pytest.mark.parametrize(
+    "student_id, body, expected_status, expected_response",
+    [
+        (
+            "nonexistent",
+            {"nonexistent_attribute": "hello"},
+            404,
+            "userId:nonexistent not found!",
+        ),  # Non existent user error
+        ("nonexistent", {}, 400, "Content of body is empty"),  # Body is empty error,
+        (
+            "nonexistent",
+            {"nonexistent_attribute": "hello", "nonexistent_attribute2": "hello"},
+            400,
+            "Send only ONE value to update, not multiple",
+        ),
+        (
+            "test-user-id",
+            {"email": "google@apple.com"},
+            200,
+            "Success! Updated the record for user-id test-user-id, email=google@apple.com",
+        ),
+    ],
+)
+@pytest.mark.benchmark(group="Manage Users")
+def test_update_user(
+    benchmark, dynamodb_setup, student_id, body, expected_status, expected_response
+):
+    event = {
+        "httpMethod": "PUT",
+        "pathParameters": {"userId": student_id},
+        "body": body,
+    }
+
+    response = benchmark(lambda_handler, event, None)
+    print(json.loads(response["body"]))
+    print(expected_response)
+    assert response["statusCode"] == expected_status
+    assert json.loads(response["body"]) == expected_response
 
 
 @pytest.mark.parametrize(
     "attributes",
     [
         {
-             "sub": "12345678-abcd-1234-abcd-123456abcdef",
-             "email": "testuser@example.com",
-             "phone_number": "+1234567890",
-             "given_name": "Test",
-             "family_name": "User",
-             "birthdate": "1990-01-01"
-         }
-    ]
+            "sub": "12345678-abcd-1234-abcd-123456abcdef",
+            "email": "testuser@example.com",
+            "phone_number": "+1234567890",
+            "given_name": "Test",
+            "family_name": "User",
+            "birthdate": "1990-01-01",
+        }
+    ],
 )
 def test_post_confirmation(dynamodb_setup, attributes):
     event = {
@@ -44,31 +142,24 @@ def test_post_confirmation(dynamodb_setup, attributes):
         "userName": attributes["sub"],
         "callerContext": {
             "awsSdkVersion": "aws-sdk-js-2.953.0",
-            "clientId": "5749fbmbs7u5plqcs45itie053"
+            "clientId": "5749fbmbs7u5plqcs45itie053",
         },
-        "request": {
-            "userAttributes": attributes
-        },
-        "response": {}
+        "request": {"userAttributes": attributes},
+        "response": {},
     }
     expected_response = {
-             "student-id": attributes["sub"],
-             "email": attributes["email"],
-             "phone": attributes["phone_number"],
-             "name": attributes["given_name"] + " " + attributes["family_name"],
-             "calendar": {
-                 "busy": []
-             },
-             "dob": attributes["birthdate"],
-             "profile": {},
-             "matchPreferences": {
-                 "enabled": False,
-                 "activities": []
-             }
-         }
+        "student-id": attributes["sub"],
+        "email": attributes["email"],
+        "phone": attributes["phone_number"],
+        "name": attributes["given_name"] + " " + attributes["family_name"],
+        "calendar": {"busy": []},
+        "dob": attributes["birthdate"],
+        "profile": {},
+        "matchPreferences": {"enabled": False, "activities": []},
+    }
     PostConfirmation(event, None)
     table = boto3.resource("dynamodb", region_name="eu-west-2").Table("users-table")
-    item = table.get_item(Key={"student-id":attributes["sub"]})
+    item = table.get_item(Key={"student-id": attributes["sub"]})
     print(item)
     assert "Item" in item
     assert item["Item"] == expected_response
