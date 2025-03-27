b0f9b9d3f595ecef7a5086a600300d58e6dcc396
svn27 <svn27@bath.ac.uk>
2025-03-27 16:02:34 +0000

    e2e for users and chats and users lambda fixed (added loads to put)

--- Backend/tests/e2e/test_chats_{chatId}_api.py
+++ Backend/tests/e2e/test_chats_{chatId}_api.py
@@ -1,7 +1,63 @@
-import requests
+import json
 
-API_URL = "https://pse8lxiuf2.execute-api.eu-west-2.amazonaws.com/prod/chats/{chatId}"
+import pytest
+from Backend.tests.test_utils import (
+    fetch_from_api,
+    post_from_api,
+    put_from_api,
+)
 
 
-def test_example_1():
-    assert True
+def test_create_profile():
+    import random
+    import string
+
+    random_chatid = "".join(
+        random.choice(string.ascii_uppercase + string.digits) for _ in range(10)
+    )
+
+    response = post_from_api(
+        f"https://mdwq3r92te.execute-api.eu-west-2.amazonaws.com/prod/chats/{random_chatid}",
+        {
+            "meetupId": "test",
+            "messages": [
+                {"userId": "test-user-id", "content": "alr", "time": "10/25/90"}
+            ],
+        },
+    )
+
+    assert response.status_code == 200
+    assert (
+        json.loads(response.text)
+        == f"Success! Created a new record for chat-id {random_chatid}"
+    )
+
+    response = post_from_api(
+        f"https://mdwq3r92te.execute-api.eu-west-2.amazonaws.com/prod/chats/{random_chatid}",
+        {
+            "meetupId": "test",
+            "messages": [
+                {"userId": "test-user-id", "content": "new msg", "time": "10/25/80"}
+            ],
+        },
+    )
+
+    assert response.status_code == 200
+    assert (
+        json.loads(response.text)
+        == f"Success! Updated the record for chat-id {random_chatid}"
+    )
+
+    response = fetch_from_api(
+        f"https://mdwq3r92te.execute-api.eu-west-2.amazonaws.com/prod/chats/{random_chatid}"
+    )
+
+    assert response.status_code == 200
+    assert json.loads(response.text) == {
+        "chat-id": random_chatid,
+        "meetup-id": "test",
+        "messages": [
+            {"userId": "test-user-id", "content": "alr", "time": "10/25/90"},
+            {"userId": "test-user-id", "content": "new msg", "time": "10/25/80"},
+        ],
+    }
