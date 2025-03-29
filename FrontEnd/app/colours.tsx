87085b7cddb1392a3533ba23a6e29a8c03da1fa5
Harry Codd <hc2306@bath.ac.uk>
2025-03-29 13:24:27 +0000

    Added colours config file

---
+++ FrontEnd/app/colours.tsx
@@ -0,0 +1,24 @@
+const colours = {
+  light: {
+    primary: '#2C3F46',
+    secondary: '#dbedf5',
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    error: '#B00020',
+    text: '#000000',
+    disabled: '#F0F0F0',
+    deselected: 'grey',
+  },
+  dark: {
+    primary: '#dbedf5',
+    secondary: '#2C3F46',
+    background: '#121212',
+    surface: '#1E1E1E',
+    error: '#CF6679',
+    text: '#FFFFFF',
+    disabled: '#373737',
+    deselected: 'grey',
+  },
+}
+  
+export default colours;
